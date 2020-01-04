fcdc0f3df6967706ab13643ad2648674ed6ef825
Radosław Ernest <radoslaw.ernest@forlagshuset.no>
2020-01-04 13:31:17 +0100

    move

--- event_move.php
+++ event_move.php
@@ -1,5 +1,18 @@
 <?php
   require_once 'db_connect.php';
+  $ins = "UPDATE events SET start = :start, end = :end WHERE id = :id";
 
+  $statement = $db->prepare($ins);
+  $statement->bindParam(':start', $_POST['newStart']);
+  $statement->bindParam(':end', $_POST['newEnd']);
+  $statement->bindParam(':id', $_POST['id']);
+  $statement->execute();
 
+  class Result {}
+  $res = new Result();
+  $res->result = 'OK';
+  $res->msg = 'Wydarzenie zaktualizowano pomyślnie';
+
+  header('Content-Type: application/json');
+  echo json_encode($res);
 ?>

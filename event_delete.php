c613811f665642b104fda60ccb9b3c92c98c7d75
Radosław Ernest <radoslaw.ernest@forlagshuset.no>
2020-01-04 12:47:59 +0100

    event delete

--- event_delete.php
+++ event_delete.php
@@ -1,5 +1,16 @@
 <?php
   require_once 'db_connect.php';
+  $ins = "DELETE FROM events WHERE id = :id";
 
+  $statement = $db->prepare($ins);
+  $statement->bindParam(':id', $_POST['id']);
+  $statement->execute();
 
+  class Result {}
+  $res = new Result();
+  $res->result = 'OK';
+  $res->message = 'Usunięcie wydarzenia powiodło się';
+
+  header('Content-Type: application/json');
+  echo json_encode($res);
 ?>

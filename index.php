e91d8e950d066acf10f7464cc544570e518237d1
ismartfridge <ismartfridge@gmail.com>
2020-01-09 21:31:04 +0100

    Work on index.php, some base

---
+++ index.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Day Planner Pro</title>
+    	<link type="text/css" rel="stylesheet" href="media/stylesheet.css" />
+</head>
+<body>
+        <div style="background: #001328 url('img/header.png') 0 0 repeat-x; padding-left: 10px;">
+			<div style="background: url('img/header-in.png') 50% 0 no-repeat; padding:1px 0; width:100%; min-height:80px">
+				<div>
+					<h1 style="font-size:200%; color:#fff; margin:10px 0 0; font-weight:bold; line-height:1.3;"><img src="media/img/logo.png" />Day Planner Pro</h1>
+					<p style="color:#fff; margin:0;">Get your day scheduled and do not miss any more of fucking boring tasks nobody else wants to do and forces you to do it</p>
+					<hr style="display: none;" />
+				</div>
+			</div>
+        </div>
+        <div style="background: url('img/main.png') 0 0 repeat-x; height: 5px;"></div>
+
+        <div style="padding: 0px 5px 0px 5px; xbackground: url('img/shadow.png') repeat-x;">
+		
+		<script type="text/javascript">
+        <!-- calendar implementation TODO -->
+		
+		</script>
+		</div>
+</body>
+</html>

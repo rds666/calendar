4348819ee5ff722d21cb1ffc5a8f0f4e642065c9
Radosław Ernest <radoslaw.ernest@forlagshuset.no>
2020-01-10 20:45:49 +0100

    main

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 ﻿<!DOCTYPE html>
-<html>
+<html lang="pl">
 <head>
-    <title>Day Planner Pro</title>
-    <link type="text/css" rel="stylesheet" href="assets/scss/style.css" />
+		<meta charset="utf-8">
+		<title>Day Planner Pro</title>
+		<link type="text/css" rel="stylesheet" href="assets/scss/style.css" />
 </head>
 <body>
-			<header class="app-header">
+		<header class="app-header">
 				<img src="assets/images/logo.png" alt="Logo Day Planner Pro">
 				<span>Day Planner Pro</span>
-			</header>
+		</header>
 
-			<main>
-			</main>
-		
+		<main>
+				<div>
+						<div>nav</div>
+				</div>
+				<div>
+						<div>dp</div>
+				</div>
+		</main>
+	
 		<script type="text/javascript">
-        <!-- calendar implementation TODO -->
-		
+				<!-- calendar implementation TODO -->
 		</script>
-		</div>
 </body>
 </html>
